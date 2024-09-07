e091246c3a016e66f56992cd96d869d934d7bb7b
momodedev <v-chengzhiz@microsoft.com>
2024-09-07 10:18:59 +0000

    Update the tf

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+# Configure backend for state management
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bucket-XXXX"
+    key            = "statefile.tfstate"
+    dynamodb_table = "terraform-state-lock-table"
+    encrypt        = true
+    profile = "oidc"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  profile = "oidc"
+}
+
 resource "aws_sqs_queue" "queue" {
   name = "sample-queue"
 }
